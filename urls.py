8ce38a64864d96d6f576af8bdb68b1d5288fa6b6
Bradley Wright <brad@intranation.com>
2008-11-30 13:36:32 +0000

    Add awesome hackage to server static files

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^$', 'views.home'),
     # Example:
     # (r'^static_mgmt/', include('static_mgmt.foo.urls')),
 
@@ -15,3 +17,20 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/(.*)', admin.site.root),
 )
+
+# this is for serving static files in development
+if settings.DEBUG:
+    import os
+    # get the static path from settings
+    static_url = settings.MEDIA_URL
+    if static_url.startswith('/'):
+        static_url = static_url.lstrip('/')
+    urlpatterns += patterns('',
+        (
+            r'^%s(?P<path>.*)$' % static_url,
+            'django.views.static.serve',
+            {
+                'document_root': settings.MEDIA_ROOT
+            }
+        ),
+    )
