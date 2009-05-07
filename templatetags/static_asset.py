0049f312cdeafb343b42a06fe24c4b63912ac2c4
Bradley Wright <brad@intranation.com>
2009-05-07 17:10:39 +0100

    Fix up static_asset call in development mode

--- templatetags/static_asset.py
+++ templatetags/static_asset.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from django import template
 from django.conf import settings
@@ -9,9 +10,15 @@ register = template.Library()
 @register.simple_tag
 def static_asset(file_name):
     """produces the full versioned path for a static asset
-
+    
     <img alt="Logo" src="{% static_asset "img/logo.png" %}>"""
-    try:
-        return settings.STATIC_MANAGEMENT_VERSIONS[file_name]
-    except AttributeError:
-        raise template.TemplateSyntaxError, "%s not in static version settings" % file_name
+    if settings.DEBUG:
+        if file_name.startswith('http://'):
+            return file_name
+        else:
+            return "%s%s?cachebust=%s" % (settings.MEDIA_URL, file_name, time.time())
+    else:
+        try:
+            return settings.STATIC_MANAGEMENT_VERSIONS[file_name]
+        except AttributeError:
+            raise template.TemplateSyntaxError, "%s not in static version settings" % file_name
