811b3545879f345253b71bbff6a075973fba0e72
Bradley Wright <brad@intranation.com>
2011-06-27 11:21:08 -0700

    Add ability to throw errors if a file is missing via STATIC_MANAGEMENT_MISSING_FILE_ERROR flag.

--- templatetags/static_asset.py
+++ templatetags/static_asset.py
@@ -1,5 +1,6 @@
 import os
 import time
+import sys
 
 from django import template
 from django.conf import settings
@@ -10,12 +11,16 @@ register = template.Library()
 @register.simple_tag
 def static_asset(file_name):
     """produces the full versioned path for a static asset
-    
+
     <img alt="Logo" src="{% static_asset "img/logo.png" %}>"""
     if settings.DEBUG:
         if file_name.startswith('http://'):
             return file_name
         else:
+            if settings.STATIC_MANAGEMENT_MISSING_FILE_ERROR:
+                path = os.path.join(settings.MEDIA_ROOT, file_name)
+                if not os.path.exists(path):
+                    raise template.TemplateSyntaxError, '%s does not exist' % os.path.abspath(path)
             return "%s%s?cachebust=%s" % (settings.MEDIA_URL, file_name, time.time())
     else:
         try:
