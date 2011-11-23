550dda23b587bc9611b748ce652c1c6259ed7c4b
Bradley Wright <brad@intranation.com>
2011-11-23 15:34:07 +0000

    Separate out formatting and listing of files into separate functions.
    The _group_file_names_and_output function now returns an ordered list of
    URLs to output into the page, rather than a concatenated string. This
    allows us to operate on the lists in templates and other places.

--- templatetags/static_combo.py
+++ templatetags/static_combo.py
@@ -10,7 +10,7 @@ register = template.Library()
 @register.simple_tag
 def static_combo_css(file_name, media=None):
     """combines files in settings
-    
+
     {% static_combo_css "css/main.css" %}"""
     # override the default if an override exists
     try:
@@ -20,43 +20,64 @@ def static_combo_css(file_name, media=None):
             link_format = '<link rel="stylesheet" type="text/css" href="%%s" media="%s">\n' % media
         else:
             link_format = '<link rel="stylesheet" type="text/css" href="%s">\n'
-    output = _group_file_names_and_output(file_name, link_format, 'css')
-    return output
+    files = static_combo_css_urls(file_name)
+    formatted_files = [link_format % filename for filename in files]
+    return "\n".join(formatted_files)
 
 @register.simple_tag
 def static_combo_js(file_name):
     """combines files in settings
-    
+
     {% static_combo_js "js/main.js" %}"""
     # override the default if an override exists
     try:
         script_format = settings.STATIC_MANAGEMENT_SCRIPT_SRC
     except AttributeError:
         script_format = '<script type="text/javascript" src="%s"></script>\n'
-    output = _group_file_names_and_output(file_name, script_format, 'js')
-    return output
+    files = static_combo_js_urls(file_name)
+    formatted_files = [script_format % filename for filename in files]
+    return "\n".join(formatted_files)
 
-def _group_file_names_and_output(parent_name, output_format, inheritance_key):
+@register.simple_tag
+def static_combo_css_urls(file_name, separator=None):
+    "Wraps static_combo_urls as a template tag"
+    return _static_combo_urls(file_name, separator, 'css')
+
+@register.simple_tag
+def static_combo_js_urls(file_name, separator=None):
+    "Wraps static_combo_urls as a template tag"
+    return _static_combo_urls(file_name, separator, 'js')
+
+def _static_combo_urls(file_name, separator=None, key='css'):
+    """Returns a list of URLs.
+    If `separator` is provided, return a string separated by the argument.
+    Otherwise returns a Python list of URLs"""
+    files = _group_file_names_and_output(file_name, key)
+    if separator:
+        return separator.join(files)
+    return files
+
+def _group_file_names_and_output(parent_name, inheritance_key):
     """helper function to do most of the heavy lifting of the above template tags"""
     try:
         file_names = settings.STATIC_MANAGEMENT[inheritance_key][parent_name]
     except AttributeError:
         raise template.TemplateSyntaxError, "%s not in static combo settings" % parent_name
-    output = ''
+    output = []
     if settings.DEBUG:
         # we need to echo out each one
         media_url = settings.MEDIA_URL
         for file_name in file_names:
             file_path = os.path.join(settings.MEDIA_ROOT, file_name)
             if file_name in settings.STATIC_MANAGEMENT[inheritance_key]:
-                output += _group_file_names_and_output(file_name, output_format, inheritance_key)
+                output = output + _group_file_names_and_output(file_name, inheritance_key)
             else:
                 if os.path.exists(file_path):
                     # need to append a cachebust as per static_asset
-                    to_output = output_format % os.path.join(settings.MEDIA_URL, file_name)
+                    to_output = os.path.join(settings.MEDIA_URL, file_name)
                     if hasattr(settings, 'STATIC_MANAGEMENT_CACHEBUST') and settings.STATIC_MANAGEMENT_CACHEBUST:
                         to_output += "?cachebust=%s" % time.time()
-                    output += to_output
+                    output.append(to_output)
                 else:
                     raise template.TemplateSyntaxError, "%s does not exist" % file_path
     else:
@@ -65,5 +86,5 @@ def _group_file_names_and_output(parent_name, output_format, inheritance_key):
         except (AttributeError, KeyError):
             raise template.TemplateSyntaxError, "%s not in static version settings" % parent_name
         # return "combined" files
-        output = output_format % "%s" % parent_name
+        output.append(parent_name)
     return output
